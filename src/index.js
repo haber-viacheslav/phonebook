1b79f96227fbb2287483ce7a6456a907e24191be
Viacheslav Haber <gaberslavik@gmail.com>
2023-07-06 17:00:02 +0300

    fixed basename of brouser router

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/phonebook">
+        <BrowserRouter basename="/haber-viacheslav/phonebook">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
